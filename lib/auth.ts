ee37df04dd27affd9c3aa5a30d684bd364a59d1b
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-09-02 22:59:13 -0400

    created mongo auth instance

---
+++ lib/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI as string);
+const db = client.db(process.env.MONGO_DB_NAME);
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db),
+});
