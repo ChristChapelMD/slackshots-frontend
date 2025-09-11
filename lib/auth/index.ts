d0124abc49abf83f54ab14ae724222771c69e64f
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-09-11 16:58:34 -0400

    added slack scopes to better auth config

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
+import { slackScopesConfig } from "@/config/scopes";
+
 const client = new MongoClient(process.env.MONGO_URI as string);
 const db = client.db(process.env.MONGO_DB_NAME);
 
@@ -14,6 +16,7 @@ export const auth = betterAuth({
     slack: {
       clientId: process.env.SLACK_CLIENT_ID as string,
       clientSecret: process.env.SLACK_CLIENT_SECRET as string,
+      scopes: slackScopesConfig,
     },
   },
   trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL as string],
