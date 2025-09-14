4996d0dd42862404edd2197d03930dacc9c9f9e8
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-09-13 23:44:18 -0400

    refer user scheme from server auth

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -1,5 +1,9 @@
+import type { auth } from "@/lib/auth";
+
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL + "/api/auth",
+  plugins: [inferAdditionalFields<typeof auth>()],
 });
