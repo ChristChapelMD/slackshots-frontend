edf997f30ad6937dde4b271c1d42fd2e6fc3c655
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-09-15 17:49:36 -0400

    restored accidently deleted workspace operation file

---
+++ services/db/operations/workspace.operation.ts
@@ -0,0 +1,49 @@
+import { Workspace, WorkspaceDTO } from "../models/workspace.model";
+
+import dbConnect from "@/services/db/connection";
+dbConnect();
+
+export async function createOrUpdateWorkspace(
+  data: WorkspaceDTO,
+): Promise<WorkspaceDTO | null> {
+  try {
+    const workspace = await Workspace.findOneAndUpdate(
+      { workspaceId: data.workspaceId },
+      { $set: data },
+      { new: true, upsert: true },
+    ).lean<WorkspaceDTO>();
+
+    return workspace;
+  } catch (error) {
+    console.error("Failed to create or update workspace:", error);
+    throw new Error("Workspace creation/update failed");
+  }
+}
+
+export async function getWorkspaceById(
+  workspaceId: string,
+): Promise<WorkspaceDTO> {
+  try {
+    const workspace = await Workspace.findOne({
+      workspaceId,
+    }).lean<WorkspaceDTO>();
+
+    if (!workspace) {
+      throw new Error(`Workspace with ID ${workspaceId} not found.`);
+    }
+
+    return workspace;
+  } catch (error) {
+    console.error("Failed to get workspace:", error);
+    throw new Error("Workspace retrieval failed");
+  }
+}
+
+export async function listAllWorkspaces(): Promise<Array<WorkspaceDTO | null>> {
+  try {
+    return await Workspace.find({}).lean<WorkspaceDTO[]>();
+  } catch (error) {
+    console.error("Failed to list workspaces:", error);
+    throw new Error("Workspace listing failed");
+  }
+}
